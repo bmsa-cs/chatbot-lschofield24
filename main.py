4901094328b2c4129a5b77d39986ece390abce3f
Lillian Schofield <lschofield24@biomedscienceacademy.org>
2021-01-21 18:13:32 +0000

    What I changed is after adding all of my templates I started to add the words to be inputted as well as outputed

--- main.py
+++ main.py
@@ -10,25 +10,54 @@ import importlib.util
 
 import random
 
+
 def run_tests():
-  """
+    """
   This function will check for the pytest module
   before calling it to run the included tests.py
   """
-  if importlib.util.find_spec('pytest') is None: # Check if pytest is installed
-    os.system('python3 -m pip install -q pytest')
+    if importlib.util.find_spec(
+            'pytest') is None:  # Check if pytest is installed
+        os.system('python3 -m pip install -q pytest')
 
-  command = "python3 -m pytest --tb=line -v -s tests.py"
-  print(command)
-  os.system(command)
+    command = "python3 -m pytest --tb=line -v -s tests.py"
+    print(command)
+    os.system(command)
+
 
 def main():
-  """This function contains all code for the chatbot."""
-  print("Hello!")
+    """This function contains all code for the chatbot."""
+    print("Hello!")
 
 
 if __name__ == "__main__":
-  main()
-  t = input("Run pytest? (y/n)").lower()
-  if t == 'y':
-    run_tests()
+    main()
+    t = input("Run pytest? (y/n)").lower()
+    if t == 'y':
+        run_tests()
+
+
+print("Hello!")
+
+#first line 
+hello = int(input("How old are you?"))
+
+if hello>=18:
+
+  print("Wow that's old! \nYou're an adult!")
+
+elif hello < 18: 
+
+  print("Wow that's young! \nYou're still a student!") 
+
+  #second line
+
+hello = input("How are you today?")
+
+if hello=="Good":
+
+  print("That is great! \nI'm glad you're doing good.")
+
+elif hello== "Bad": 
+
+  print("Oh no, I'm sorry! \nI hope it gets better.") 
